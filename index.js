5ce5675af0afe6199ed89dada473d716f5bdd7db
TomGoldberg <tomg3210@gmail.com>
2020-03-24 13:04:20 +0200

    Server index.js update - single row function filter 2

--- index.js
+++ index.js
@@ -4,31 +4,27 @@ const express = require('express'),
     port = process.env.PORT || 3000;
 
 // app.use('/',express.static(`${__dirname}/html/views`));
-app.use(express.static(__dirname+'/html/assets'));
+app.use(express.static(__dirname + '/html/assets'));
 app.set('/', __dirname + '/html/views');
 app.set('view engine', 'pug');
 
-app.use( (req,res,next) => {
-    res.header("Access-Control-Allow-Origin","*");
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-app.get('/dog/:id', (req,res)=>{
-   let id = req.params.id;
+app.get('/dog/:id', (req, res) => {
+    let id = req.params.id,
+        dog = data.data.filter(e => e.id == id);
 
-   let dog = data.data.filter((e,d)=>{
-       if(e.id == id)
-           return e;
-   });
-
-    res.render('index',{
-        'd_name' : dog[0].name,
-        'd_txt' : dog[0].text,
-        'd_img' : dog[0].img,
-        'dog' : dog[0],
-        'data' : data.data,
-        'length' : data.data.length
+    res.render('index', {
+        'd_name': dog[0].name,
+        'd_txt': dog[0].text,
+        'd_img': dog[0].img,
+        //'dog': dog[0],
+        'data': data.data,
+        'length': data.data.length
     });
 });
 
